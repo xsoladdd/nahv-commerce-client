ae19c75ded9823020bc7f84b402730e8a5f78b97
Ericson Funtanar <efuntanar.cloudpanda@gmail.com>
2021-06-24 14:18:52 +0800

    UPDATE: Fix issues on change repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./layout/Layout";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+interface AppProps {}
+
+const queryClient = new QueryClient();
+
+const App: React.FC<AppProps> = ({}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route>
+            <Layout />
+          </Route>
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
-}
-
+};
 export default App;
